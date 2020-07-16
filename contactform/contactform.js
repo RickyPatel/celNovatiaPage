be3a7e67b08669a89e0667765bbd22c19cf74e61
RickyPatel <rickypatel1710@gmail.com>
2020-07-16 18:24:37 +0530

    adding contact form js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,4 +1,3 @@
-// of coalescence
 // Your web app's Firebase configuration
  var firebaseConfig = {
   apiKey: "AIzaSyBMgKoISM8MJdr88ARHq_KQj9w5lXwoaWk",
@@ -11,25 +10,25 @@
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-  var messageRef = firebase.database().ref('message');
+
+  var messageRef = firebase.database().ref('message');          
   
-  document.getElementById('contact_form').addEventListener('submit',submitform);
+  document.getElementById('contact_form').addEventListener('submit',submitform); 
   
-  function getIdValue(id)
-  {
-      return document.getElementById(id).value;
-  }
+  function getIdValue(id) 
+  {                                             
+      return document.getElementById(id).value;     
+  }                                                         
   
-  function submitform(e)
-  {
-      e.preventDefault();
-      console.log(123);
+  function submitform(e)    
+  {                                                 
+      e.preventDefault();                           
   
-      var name = getIdValue('name');
-      var email = getIdValue('email');
-      var message = getIdValue('message');
+      var name = getIdValue('name');                    
+      var email = getIdValue('email');                            
+      var message = getIdValue('message');                  
   
-      console.log(name);
+      console.log(name);        
       
       saveMessage(name,email,message);
   
@@ -41,11 +40,12 @@ firebase.initializeApp(firebaseConfig);
   
       setTimeout(function(){
           document.querySelector('.alert').style.display = 'none';
-      },3000)
-  
+      },3000);
+
+      document.getElementById('contact_form').reset();
   }
   
-  function saveMessage(name,email,message)
+  function saveMessage(name,email,message)                              
   {
       var newMessageRef = messageRef.push();
       newMessageRef.set({
@@ -54,127 +54,3 @@ firebase.initializeApp(firebaseConfig);
           message: message
       })
   }
-
-
-
-
-
-//of novatia
-// jQuery(document).ready(function($) {
-//   "use strict";
-
-//   //Contact
-//   $('form.contactForm').submit(function() {
-//     var f = $(this).find('.form-group'),
-//       ferror = false,
-//       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
-
-//     f.children('input').each(function() { // run all inputs
-
-//       var i = $(this); // current input
-//       var rule = i.attr('data-rule');
-
-//       if (rule !== undefined) {
-//         var ierror = false; // error flag for current input
-//         var pos = rule.indexOf(':', 0);
-//         if (pos >= 0) {
-//           var exp = rule.substr(pos + 1, rule.length);
-//           rule = rule.substr(0, pos);
-//         } else {
-//           rule = rule.substr(pos + 1, rule.length);
-//         }
-
-//         switch (rule) {
-//           case 'required':
-//             if (i.val() === '') {
-//               ferror = ierror = true;
-//             }
-//             break;
-
-//           case 'minlen':
-//             if (i.val().length < parseInt(exp)) {
-//               ferror = ierror = true;
-//             }
-//             break;
-
-//           case 'email':
-//             if (!emailExp.test(i.val())) {
-//               ferror = ierror = true;
-//             }
-//             break;
-
-//           case 'checked':
-//             if (! i.is(':checked')) {
-//               ferror = ierror = true;
-//             }
-//             break;
-
-//           case 'regexp':
-//             exp = new RegExp(exp);
-//             if (!exp.test(i.val())) {
-//               ferror = ierror = true;
-//             }
-//             break;
-//         }
-//         i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-//       }
-//     });
-//     f.children('textarea').each(function() { // run all inputs
-
-//       var i = $(this); // current input
-//       var rule = i.attr('data-rule');
-
-//       if (rule !== undefined) {
-//         var ierror = false; // error flag for current input
-//         var pos = rule.indexOf(':', 0);
-//         if (pos >= 0) {
-//           var exp = rule.substr(pos + 1, rule.length);
-//           rule = rule.substr(0, pos);
-//         } else {
-//           rule = rule.substr(pos + 1, rule.length);
-//         }
-
-//         switch (rule) {
-//           case 'required':
-//             if (i.val() === '') {
-//               ferror = ierror = true;
-//             }
-//             break;
-
-//           case 'minlen':
-//             if (i.val().length < parseInt(exp)) {
-//               ferror = ierror = true;
-//             }
-//             break;
-//         }
-//         i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-//       }
-//     });
-//     if (ferror) return false;
-//     else var str = $(this).serialize();
-//     var action = $(this).attr('action');
-//     if( ! action ) {
-//       action = 'contactform/contactform.php';
-//     }
-//     $.ajax({
-//       type: "POST",
-//       url: action,
-//       data: str,
-//       success: function(msg) {
-//         // alert(msg);
-//         if (msg == 'OK') {
-//           $("#sendmessage").addClass("show");
-//           $("#errormessage").removeClass("show");
-//           $('.contactForm').find("input, textarea").val("");
-//         } else {
-//           $("#sendmessage").removeClass("show");
-//           $("#errormessage").addClass("show");
-//           $('#errormessage').html(msg);
-//         }
-
-//       }
-//     });
-//     return false;
-//   });
-
-// });
